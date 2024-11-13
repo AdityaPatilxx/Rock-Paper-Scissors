cc3521fad0d2487ace7bb4c8f2acfba6f83aa206
Aditya <161197993+AdityaPatilxx@users.noreply.github.com>
2024-11-13 11:37:48 +0530

    feat: add humChoice function
    -humChoice function take user input and returns value between rock, paper and scissor depending on the input
    -added global constants for rock,paper and scissor to shorten the committed
    -updated compChoice to use added global constants

--- script.js
+++ script.js
@@ -4,18 +4,40 @@ const SCISSOR = 'scissor'
 
 
 function compChoice(){
-    let computerChoice
+
     let randNum = Math.floor(Math.random() * 10) + 1
     if (randNum < 7 && randNum > 3){
-        computerChoice = PAPER
+        return PAPER
     }
     else if (randNum < 4){
-        computerChoice = ROCK
+        return ROCK
     }
     else{
-        computerChoice = SCISSOR
+        return SCISSOR
     }
-    return computerChoice
+}
+
+function humChoice(){
+    let validInput =  false;
+    while(!validInput){
+        console.log('type 1 for rock, 2 for paper and 3 for scissor')
+        let userInput = prompt('What\'s your choice:')
+        if (userInput == 1){
+            validInput = true
+            return ROCK
+        }
+        else if(userInput == 2){
+            validInput = true
+            return PAPER
+        }
+        else if(userInput == 3){
+            validInput = true
+            return SCISSOR
+        }
+        else{
+            console.log('Invalid Input, try again')
+        }
+    }
 }
 
 
