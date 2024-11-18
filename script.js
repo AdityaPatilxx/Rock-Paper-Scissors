8ff201882cab7132c6f74e04e1750ff4d9fb83c1
Aditya <161197993+AdityaPatilxx@users.noreply.github.com>
2024-11-18 16:42:46 +0530

    feat:rework the script file to respond to buttons
    -rewrite playRound function to start after a button press
    -update score after every round

--- script.js
+++ script.js
@@ -2,87 +2,111 @@ const ROCK = 'rock'
 const PAPER = 'paper'
 const SCISSOR = 'scissor'
 
-let humanScore = 0
-let computerScore = 0
 
-function getComputerChoice(){
+let currentHScore = 0
+let currentCScore = 0
+
+
+function getComputerChoice() {
 
     let randNum = Math.floor(Math.random() * 10) + 1
-    if (randNum < 7 && randNum > 3){
+    if (randNum < 7 && randNum > 3) {
         return PAPER
     }
-    else if (randNum < 4){
+    else if (randNum < 4) {
         return ROCK
     }
-    else{
+    else {
         return SCISSOR
     }
 }
 
-function getHumanChoice(){
-    let validInput =  false;
-    while(!validInput){
-        let userInput = prompt('What\'s your choice:')
-        if (userInput == 1){
-            validInput = true
-            return ROCK
-        }
-        else if(userInput == 2){
-            validInput = true
-            return PAPER
-        }
-        else if(userInput == 3){
-            validInput = true
-            return SCISSOR
-        }
-        else{
-            console.log('Invalid Input, try again')
+function playRound(event) {
+
+    let target = event.target
+    let button = target.closest('button')
+
+    let humanScore = document.querySelector('#human-score')
+    let computerScore = document.querySelector('#computer-score')
+    currentHScore = parseInt(humanScore.textContent, 10)
+    currentCScore = parseInt(computerScore.textContent, 10)
+
+    let computerChoice = getComputerChoice()
+
+    if (button) {
+        switch (button.className) {
+            case 'rock':
+                console.log('rock');
+                humanChoice = ROCK
+                break;
+            case 'paper':
+                console.log('paper')
+                humanChoice = PAPER
+                break
+            case 'scissor':
+                console.log('scissor')
+                humanChoice = SCISSOR
+                break
+
         }
     }
+
+    signCompare(humanChoice, computerChoice)
+    humanScore.textContent = currentHScore
+    computerScore.textContent = currentCScore
+
 }
 
-function playRound(HC,CC){
-    if ( (HC == ROCK && CC == SCISSOR) || (HC == PAPER && CC == ROCK) || (HC == SCISSOR && CC == PAPER) ) {
+function signCompare(HC, CC) {
+    if ((HC == ROCK && CC == SCISSOR) || (HC == PAPER && CC == ROCK) || (HC == SCISSOR && CC == PAPER)) {
         console.log('You win this round!')
-        humanScore++
+        currentHScore++
+
     }
-    else if ( (HC == ROCK && CC == PAPER) || (HC == PAPER && CC == SCISSOR) || (HC == SCISSOR && CC == ROCK) ) {
+    else if ((HC == ROCK && CC == PAPER) || (HC == PAPER && CC == SCISSOR) || (HC == SCISSOR && CC == ROCK)) {
         console.log('You Lose this round!')
-        computerScore++
+        currentCScore++
     }
-    else if (HC == CC){
+    else if (HC == CC) {
         console.log('It\'s a Tied for this round')
     }
 }
 
-function playGame(){
-    let gameOn = true
-    let counter = 1
+// function playGame() {
+//     let gameOn = true
+//     let counter = 1
 
-    console.log('Welcome to game of ROCK-PAPER-SCISSOR')
-    console.log('type 1 for rock, 2 for paper and 3 for scissor')
-    while (gameOn){
-        let humanChoice = getHumanChoice()
-        let computerChoice = getComputerChoice()
-        console.log(`Round ${counter}`)
-        playRound(humanChoice, computerChoice)
-        counter++
-        if (counter > 5){
-            gameOn = false
-        }
-    }
-    console.log(`Your score: ${humanScore}`)
-    console.log(`Opponent score: ${computerScore}`)
-    if (humanScore == computerScore){
-        console.log('The game was a tie')
-    }
-    else if(humanScore > computerScore){
-        console.log('You have WON the game')
-    }
-    else{
-        console.log('You have LOST the game')
-    }
+//     console.log('Welcome to game of ROCK-PAPER-SCISSOR')
+//     console.log('type 1 for rock, 2 for paper and 3 for scissor')
+//     while (gameOn) {
+//         let humanChoice = getHumanChoice()
+//         let computerChoice = getComputerChoice()
+//         console.log(`Round ${counter}`)
+//         playRound(humanChoice, computerChoice)
+//         counter++
+//         if (counter > 5) {
+//             gameOn = false
+//         }
+//     }
+//     console.log(`Your score: ${humanScore}`)
+//     console.log(`Opponent score: ${computerScore}`)
+//     if (humanScore == computerScore) {
+//         console.log('The game was a tie')
+//     }
+//     else if (humanScore > computerScore) {
+//         console.log('You have WON the game')
+//     }
+//     else {
+//         console.log('You have LOST the game')
+//     }
 
-}
+// }
 
-playGame()
+// playGame()
+
+
+let choices = document.querySelector('#choices')
+
+choices.addEventListener('click', playRound)
+
+
