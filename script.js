02b9c5d3df031182c568ed7fcb1f5f0690da3f0c
Aditya <161197993+AdityaPatilxx@users.noreply.github.com>
2024-11-18 22:55:11 +0530

    feat: add function to show player and ai choice with the outcome
    -cancelled favorite move and match stats features that were planned
    -updated the color placement and added outline to major parts
    -if not mention in the last commit: added score display

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ function getComputerChoice() {
 
     let randNum = Math.floor(Math.random() * 10) + 1
     if (randNum < 7 && randNum > 3) {
+        addToHistory('-parchment', 0)
         return PAPER
     }
     else if (randNum < 4) {
+        addToHistory('-stone', 0)
         return ROCK
     }
     else {
+        addToHistory('-cross blades', 0)
         return SCISSOR
     }
 }
@@ -31,25 +34,26 @@ function playRound(event) {
     currentHScore = parseInt(humanScore.textContent, 10)
     currentCScore = parseInt(computerScore.textContent, 10)
 
-    let computerChoice = getComputerChoice()
-
     if (button) {
         switch (button.className) {
             case 'rock':
-                console.log('rock');
+                addToHistory('-stone', 1)
                 humanChoice = ROCK
                 break;
             case 'paper':
-                console.log('paper')
+                addToHistory('-parchment', 1)
                 humanChoice = PAPER
                 break
             case 'scissor':
-                console.log('scissor')
+                addToHistory('-cross blades', 1)
                 humanChoice = SCISSOR
                 break
 
         }
     }
+    let computerChoice = getComputerChoice()
+
+    displayChoices(humanChoice, computerChoice)
 
     choiceCompare(humanChoice, computerChoice)
     humanScore.textContent = currentHScore
@@ -59,27 +63,72 @@ function playRound(event) {
 
 function choiceCompare(HC, CC) {
     if ((HC == ROCK && CC == SCISSOR) || (HC == PAPER && CC == ROCK) || (HC == SCISSOR && CC == PAPER)) {
-        display('Victory')
+        displayOutcome('Victory')
         currentHScore++
 
     }
     else if ((HC == ROCK && CC == PAPER) || (HC == PAPER && CC == SCISSOR) || (HC == SCISSOR && CC == ROCK)) {
-        display('Defeat')
+        displayOutcome('Defeat')
         currentCScore++
     }
     else if (HC == CC) {
-        display('Draw')
+        displayOutcome('Draw')
     }
 }
 
-function display(outcome) {
+function displayOutcome(outcome) {
     let result = document.createElement('h1')
+    let screen = document.querySelector('.game-display')
+    // screen.replaceChildren()
+
+    result.textContent = outcome
+    screen.prepend(result)
+}
+
+function displayChoices(humanChoice, computerChoice) {
+    let HChoiceImage = document.createElement('img')
+    let verses = document.createElement('p')
+    let CChoiceImage = document.createElement('img')
+    let division = document.createElement('div')
+
     let screen = document.querySelector('.game-display')
     screen.replaceChildren()
 
-    result.textContent = outcome
+    if (humanChoice == ROCK) {
+        HChoiceImage.src = 'assets/images/rune_stone.png'
+    }
+    else if (humanChoice == PAPER) {
+        HChoiceImage.src = 'assets/images/parchment.png'
+    }
+    else if (humanChoice == SCISSOR) {
+        HChoiceImage.src = 'assets/images/sword-new.png'
+    }
 
-    screen.appendChild(result)
+    if (computerChoice == ROCK) {
+        CChoiceImage.src = 'assets/images/rune_stone.png'
+    }
+    else if (computerChoice == PAPER) {
+        CChoiceImage.src = 'assets/images/parchment.png'
+    }
+    else if (computerChoice == SCISSOR) {
+        CChoiceImage.src = 'assets/images/sword-new.png'
+    }
+
+    verses.textContent = 'VS'
+
+    division.append(HChoiceImage, verses, CChoiceImage)
+    screen.appendChild(division)
+}
+
+function addToHistory(elementToAdd, whereToAdd) {
+    const container = whereToAdd
+        ? document.querySelector('.human .history')
+        : document.querySelector('.computer .history');
+
+    let symbol = document.createElement('p')
+    symbol.textContent = elementToAdd
+    container.prepend(symbol)
+
 }
 
 let choices = document.querySelector('#choices')
